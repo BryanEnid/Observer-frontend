1cf6a839d4ae7d0fdc0a27473bef64ea16741957
Bryan Enid <30734480+BryanEnid@users.noreply.github.com>
2021-05-15 13:01:08 -0400

    initial

--- jest.setup.js
+++ jest.setup.js
@@ -1,26 +1,31 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
+// // jest-dom adds custom jest matchers for asserting on DOM nodes.
+// // allows you to do things like:
+// // expect(element).toHaveTextContent(/react/i)
+// // learn more: https://github.com/testing-library/jest-dom
+// import "@testing-library/jest-dom/extend-expect";
 
-// Mock i18next
-// import "./i18n";
+// // Mock i18next
+// // import "./i18n";
 
-// add this to load font awesome icons for tests.
-// import registerFaIcons from "./assets/RegisterFaIcons.js";
-// registerFaIcons();
+// // add this to load font awesome icons for tests.
+// // import registerFaIcons from "./assets/RegisterFaIcons.js";
+// // registerFaIcons();
 
-// Make development environment variables available in tests
-require("dotenv").config({ path: "./.env.development" });
+// // Make development environment variables available in tests
+// require("dotenv").config({ path: "./.env.development" });
 
-// Mock scrollTo function
-window.scrollTo = jest.fn();
+// // Mock scrollTo function
+// window.scrollTo = jest.fn();
 
-// Stub console.error and console.warn so that any React warnings need to be fixed in order for our test suite to pass
-// console.error = (err) => {
-//   throw new Error(err);
-// };
-// console.warn = (warning) => {
-//   throw new Error(warning);
-// };
+// // Stub console.error and console.warn so that any React warnings
+// // need to be fixed in order for our test suite to pass
+// // console.error = (err) => {
+// //   throw new Error(err);
+// // };
+// // console.warn = (warning) => {
+// //   throw new Error(warning);
+// // };
+
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
--- src/screen_prototypes/AsyncStorage.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export default function AsyncStorageScreen() {
-  return;
-}
