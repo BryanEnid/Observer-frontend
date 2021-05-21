1b87e12fcbbac5064a1346903f65371122db792a
Bryan Enid <30734480+BryanEnid@users.noreply.github.com>
2021-05-20 22:57:34 -0400

    prototyping screens

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
-import { Profile } from './src/screen_prototypes';
+import { Profile as PrototypingScreen } from './src/screen_prototypes';
 
 // const styles = StyleSheet.create({
 //   container: {
@@ -12,11 +12,5 @@ import { Profile } from './src/screen_prototypes';
 // });
 
 export default function App() {
-  return <Profile />;
-
-  // return (
-  //   <View style={styles.container}>
-  //     <Text>Open up App.js to start working on your app!</Text>
-  //   </View>
-  // );
+  return <PrototypingScreen />;
 }
