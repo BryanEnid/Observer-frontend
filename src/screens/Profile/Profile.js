04c99e6683e0e0125cb1354b0f2ff99a0edd518d
Bryan Enid <30734480+BryanEnid@users.noreply.github.com>
2021-06-21 23:02:24 -0400

    Refactor Carrousel and added animation

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,154 +1,80 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
   View,
-  Image,
   StyleSheet,
-  SafeAreaView,
   ScrollView,
   TouchableOpacity,
   FlatList,
   Animated,
+  Dimensions,
+  Easing,
 } from 'react-native';
-import ActionSheet from 'react-native-actions-sheet';
-import { getStatusBarHeight } from 'react-native-status-bar-height';
-import { ConicalGradient, Carousel, Text, Divider, Portal } from '../../components';
-import { Navigation } from '../../controllers';
+import * as Haptics from 'expo-haptics';
+import { Text, Divider } from '../../components';
 import DummyData from '../../controllers/DummyDataController';
 import { profileMock } from './mocks';
 import elements from './CarouselElements';
+import Header from './Header';
+import screens from '..';
 
-// Platform Fixes
-const statusBarHeight = getStatusBarHeight();
+const screenWidth = Dimensions.get('window').width;
 
 // Styles
-const profileSize = { width: 180, height: 180, padding: 20 };
+const subScreenSize = { width: screenWidth };
+const carrouselItemSize = { width: 100 };
+
 // const { width, height, padding } = profileSize;
 const styles = StyleSheet.create({
-  profile_container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: statusBarHeight,
-  },
-  profile_item: { marginBottom: 20 },
-  profile_picture: {
-    width: profileSize.width - profileSize.padding,
-    aspectRatio: 1,
-    borderRadius: profileSize.width - profileSize.padding / 2,
-  },
-  profile_description: { textAlign: 'center' },
   profile_name: {
     paddingTop: 44,
     textAlign: 'center',
   },
   profile_username: { textAlign: 'center' },
-  footer: {
-    height: 100,
+  screens: {
+    width: subScreenSize.width,
+    height: subScreenSize.width, // TODO: remove this
+  },
+  carrousel: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    marginLeft: screenWidth / 2 - carrouselItemSize.width / 2,
+  },
+  profile_item: { marginBottom: 10 },
+  carrousel_item: {
+    width: carrouselItemSize.width,
+    textAlign: 'center',
+    paddingVertical: 10,
   },
 });
-
-function Header({ profile, user, video: videoURI }) {
-  const actionSheetRef = useRef();
-  const scrollViewRef = useRef();
-
-  const onClose = useCallback(() => {
-    scrollViewRef.current?.setNativeProps({
-      scrollEnabled: false,
-    });
-  }, []);
-
-  const onOpen = useCallback(() => {
-    scrollViewRef.current?.setNativeProps({
-      scrollEnabled: true,
-    });
-  }, []);
-
-  const handleProfileVideo = useCallback(() => {
-    Navigation.navigate('ProfileVideo', { uri: videoURI });
-  }, []);
-
-  const handleProfileOption = useCallback(() => {
-    actionSheetRef.current?.show();
-  });
-
-  return (
-    <>
-      <View style={styles.profile_container}>
-        <SafeAreaView>
-          <TouchableOpacity onPress={handleProfileVideo} onLongPress={handleProfileOption}>
-            <View style={{ height: profileSize.height, ...styles.profile_item }}>
-              <ConicalGradient>
-                <Image source={{ uri: profile?.picture?.large }} style={styles.profile_picture} />
-              </ConicalGradient>
-            </View>
-          </TouchableOpacity>
-        </SafeAreaView>
-
-        <View>
-          <Text variant="caption" style={styles.profile_description}>
-            {`Software Engineer at Facebook\n"${user.quote}"`}
-          </Text>
-        </View>
-      </View>
-
-      <ActionSheet
-        initialOffsetFromBottom={0.6}
-        ref={actionSheetRef}
-        onOpen={onOpen}
-        statusBarTranslucent
-        bounceOnOpen
-        bounciness={4}
-        gestureEnabled
-        onClose={onClose}
-        defaultOverlayOpacity={0.3}
-      >
-        <View style={{ paddingHorizontal: 12 }}>
-          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Text variant="h2">option 1</Text>
-            <Text variant="h2">option 2</Text>
-            <Text variant="h2">option 3</Text>
-          </View>
-
-          <View style={styles.footer} />
-        </View>
-      </ActionSheet>
-    </>
-  );
-}
-
-function Sticky({ profile }) {
-  return (
-    <>
-      <View style={{ justifyContent: 'center', backgroundColor: 'white' }}>
-        <View style={styles.profile_item}>
-          <Text variant="h2" style={styles.profile_name}>
-            {profile.name.first} {profile.name.last}
-          </Text>
-          <Text variant="caption" style={styles.profile_username}>
-            @{profile.login.username}
-          </Text>
-        </View>
-
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Divider style={styles.profile_item} />
-        </View>
-
-        <View style={styles.profile_item}>
-          {/* <Carousel elements={elements} gateNameRender="ProfileCarouselScreens" /> */}
-        </View>
-      </View>
-    </>
-  );
-}
 
 export default function Profile() {
   const [profile, setProfile] = useState(profileMock);
   const [video, setVideo] = useState({});
+  const subScreens = useRef(null);
+
+  const threshold = useRef(false);
   const activeIndex = useRef(new Animated.Value(0)).current;
-  // const activeIndexAnimation = useRef(new Animated.Value(0)).current;
+  const activeIndexAnimation = useRef(new Animated.Value(0)).current;
   const [user] = useState({
     quote: 'Seagulls are the eagles of the sea.',
   });
-  const selectedItemIndex = 0;
+
+  const size = carrouselItemSize.width;
+  const translateX = activeIndexAnimation.interpolate({
+    inputRange: [-1, 0, 1],
+    outputRange: [size, 0, -size],
+  });
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(activeIndexAnimation, {
+        toValue: activeIndex,
+        duration: 500,
+        easing: Easing.out(Easing.exp),
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
 
   useEffect(() => {
     (async () => {
@@ -160,8 +86,6 @@ export default function Profile() {
         setVideo(profileVideo);
         setProfile(profileDetails);
       } catch (e) {
-        // console.log(e.message);
-        // console.log(e);
         setProfile(profileMock);
       }
     })();
@@ -172,22 +96,59 @@ export default function Profile() {
       <ScrollView stickyHeaderIndices={[1]}>
         <Header profile={profile} user={user} video={video} />
 
-        <Sticky profile={profile} />
+        <View style={{ justifyContent: 'center', backgroundColor: 'white' }}>
+          <View style={styles.profile_item}>
+            <Text variant="h2" style={styles.profile_name}>
+              {profile.name.first} {profile.name.last}
+            </Text>
+            <Text variant="caption" style={styles.profile_username}>
+              @{profile.login.username}
+            </Text>
+          </View>
 
-        {/* See Carrousel Component for logic
-          See CarrouselScreensSetup for registering screens */}
-        {/* <Portal name="ProfileCarouselScreens" /> */}
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Divider style={styles.profile_item} />
+          </View>
+
+          <Animated.View style={[styles.carrousel, { transform: [{ translateX }] }]}>
+            {elements.map((item, index) => (
+              <TouchableOpacity
+                key={item.title.trim().toLowerCase()}
+                onPress={() => {
+                  activeIndex.setValue(index);
+                  subScreens.current.scrollToIndex({ index, animated: true });
+                }}
+              >
+                <Text style={styles.carrousel_item}>{item.title}</Text>
+              </TouchableOpacity>
+            ))}
+          </Animated.View>
+        </View>
+
         <FlatList
           data={elements}
           onMomentumScrollEnd={(ev) => {
-            const newIndex = Math.floor(ev.nativeEvent.contentOffset.x / width);
+            const newIndex = Math.floor(ev.nativeEvent.contentOffset.x / subScreenSize.width);
+            // TODO: It should not vibrate when prevIndex is same as activeIndex
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
             activeIndex.setValue(newIndex);
           }}
+          ref={subScreens}
+          showsHorizontalScrollIndicator={false}
           horizontal
           pagingEnabled
-          initialScrollIndex={selectedItemIndex}
-          getItemLayout={(_, index) => ({ length: width, offset: width * index, index })}
-          renderItem={() => {}}
+          initialScrollIndex={0}
+          getItemLayout={(_, index) => ({
+            length: subScreenSize.width,
+            offset: subScreenSize.width * index,
+            index,
+          })}
+          keyExtractor={(item) => item.title.trim().toLowerCase()}
+          renderItem={({ item: { component: Component } }) => (
+            <View style={styles.screens}>
+              <Component />
+            </View>
+          )}
         />
       </ScrollView>
     </View>
